485e7d3dd8c0e198a1100fa8a84cc3ab8e12bad2
Hafizh <hafizh.raharja20@gmail.com>
2023-09-12 11:19:20 +0700

    Laravel1

---
+++ app/Http/Controllers/HafizhController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HafizhController extends Controller
+{
+    public function index()
+    {
+        return view('Hafizh.index');
+    }
+    //
+}
---
+++ resources/views/Hafizh/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="background-color: #000117; font-family: Arial; height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center">
+    <h1 style="color: white; font-size: 10rem; margin: 0;">WEB HAFIZH</h1>
+    <p style="color: white; font-size: 7rem; margin: 0;">XII RPL 1</p>
+    <a href="#" style="color: white; background-color: rgb(178, 0, 0); padding: 1em 2em; border-radius: 10px; text-decoration: none; font-weight: bold; margin-top: 3em; font-size: 1.5rem">Lihat selengkapnya</a>
+</body>
+</html>
